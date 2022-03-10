e1267378ad96cacefad27b43cdfbe0e0cdd9e49f
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-03-10 20:46:08 +0700

    fix ez

--- Structural Pattern/adapter/ShapeAdapter/Program.cs
+++ Structural Pattern/adapter/ShapeAdapter/Program.cs
@@ -4,23 +4,20 @@
     {
         public static void Main()
         {
-            RoundHole hole = new RoundHole(10);
+            RoundHole hole = new RoundHole(5);
             RoundNail roundNail = new RoundNail(5);
 
             hole.fits(roundNail);
             Console.WriteLine("-------------------\n");
 
             SquareNail smallSquareNail = new SquareNail(5);
-            SquareNail largeSquareNail = new SquareNail(15);
 
             hole.fits(smallSquareNail);
             Console.WriteLine("-------------------\n");
 
             RoundNail smallSqualeNailAdapter = new SquareNailAdapter(smallSquareNail);
-            RoundNail largeSqualeNailAdapter = new SquareNailAdapter(largeSquareNail);
             Console.WriteLine("-------------------");
             hole.fits(smallSqualeNailAdapter);
-            hole.fits(largeSqualeNailAdapter);
 
 
         }
