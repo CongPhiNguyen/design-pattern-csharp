47a1273606cc4d8c3c029909c5b017c90d1ad1de
UIT19522006 <UIT19522006@gm.uit.edu.vn>
2022-05-06 01:47:28 +0700

    sample meditor and memento

---
+++ Behavioral Pattern/mediator/Mediator/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Mediator
+{
+    public class MainApp
+    {
+        public static void Main()
+        {
+            ConcreteMediator m = new ConcreteMediator();
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
+        }
+    }
+}
---
+++ Behavioral Pattern/memento/Memento/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Memento
+{
+    public class MainApp
+    {
+        public static void Main()
+        {
+            Originator o = new Originator();
+            o.State = "On";
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
+            o.State = "Off";
+            o.SetMemento(c.Memento);
+        }
+    }
+}
